fce58f930c7ec6caaf186ae9e34e3b33c3295d6a
Ailullah47 <alishafaie47@gmail.com>
2023-05-17 07:21:19 +0200

    last changes

--- src/components/HeaderPage.jsx
+++ src/components/HeaderPage.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 export default function HeaderPage() {
   return (
-    <div className='flex justify-'>
+    <div className=' max-w-7xl mx-auto items-center py-4 px-2 sticky top-0'>
+
+    <div className=' flex  justify-between '>
         <div>Real website</div>
-        <div >
-            <ul>
-                <li>offer</li>
+        <div className='text-gray-300' >
+            <ul  className='flex space-x-2 md:space-x-6 lg:space-x-10 cursor-pointer'>
+                <li>Home</li>
+                <li>Offers</li>
                 <li>Sign In</li>
-                <li></li>
             </ul>
         </div>
+    </div>
     </div>
   )
 }
